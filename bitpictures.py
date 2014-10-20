1b293f52ba67bee485ad759f713f528c524e67f7
obskyr <powpowd@gmail.com>
2014-10-21 00:47:47 +0200

    Fixed support for formats without alpha channel

--- bitpictures.py
+++ bitpictures.py
@@ -4,14 +4,23 @@ import bitpix
 import bpconfig
 from PIL import Image
 
-def bitmapToImage(bitmap):
-    alpha = Image.new('1', bitmap.size, 0)
-    image = Image.new('RGBA', bitmap.size, (0, 0, 0))
+def bitmapToImage(bitmap, alpha=True):
+    if alpha:
+        mode = 'RGBA'
+        alphaChannel = Image.new('1', bitmap.size, 0)
+    else:
+        mode = 'RGB'
+    image = Image.new(mode, bitmap.size, (0, 0, 0))
     for y, row in enumerate(bitmap.colorMatrix):
         for x, color in enumerate(row):
-            alpha.putpixel((x, y), color[3])
+            if alpha:
+                try:
+                    alphaChannel.putpixel((x, y), color[3])
+                except IndexError:
+                    alphaChannel.putpixel((x, y), 1)
             image.putpixel((x, y), color[:3])
-    image.putalpha(alpha)
+    if alpha:
+        image.putalpha(alphaChannel)
     return image
 
 if __name__ == '__main__':
@@ -30,9 +39,24 @@ if __name__ == '__main__':
     except IndexError:
         configPath = 'config.cfg'
 
+    # Some formats have commonly used alternate extensions.
+    altExtensions = {
+        'jpg': 'jpeg',
+        'tif': 'tiff',
+        'dib': 'bmp'
+    }
+    # Not the formats that actually support an alpha channel, but...
+    # the formats that PIL won't raise an error saving an RGBA image as.
+    alphaFormats = ['png', 'jpeg', 'gif', 'ppm', 'tiff', 'webp']
+    defaultFormat = 'bmp'
+
     extensionRe = re.compile(r"^.*\.([a-zA-Z0-9]+)$")
     outFormat = re.search(extensionRe, outPath)
     outFormat = outFormat.group(1) if outFormat != None else outFormat
+    outFormat = outFormat.lower()
+    outFormat = altExtensions[outFormat] if outFormat in altExtensions else outFormat
+
+    alpha = outFormat in alphaFormats
 
     def parseColorString(colorString):
         return tuple(int(v) for v in re.split(r"\s*,\s*", colorString))
@@ -40,9 +64,8 @@ if __name__ == '__main__':
     bpconfig.colorParser = parseColorString # To get the tuple format.
     config = bpconfig.getConfig(configPath)
     bitmap = bitpix.Bitmap(inPath, config)
-    image = bitmapToImage(bitmap)
-
+    image = bitmapToImage(bitmap, alpha)
     try:
         image.save(outPath, outFormat)
     except KeyError:
-        image.save(outPath)
+        image.save(outPath, defaultFormat)
